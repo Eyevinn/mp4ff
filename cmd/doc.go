b27cc6d5b8d0b9570bac2d649856125e534276b7
Torbjörn Einarsson <torbjorn.einarsson@edgeware.tv>
2020-12-27 12:31:46 +0100

    doc: add cmd/doc.go

---
+++ cmd/doc.go
@@ -0,0 +1,10 @@
+/*
+Package cmd - command line tools built using mp4ff.
+
+Install like
+
+	go install github.com/edgeware/mp4ff/cmd/mp4ff-info
+	go install github.com/edgeware/mp4ff/cmd/mp4ff-pslister
+
+*/
+package cmd
