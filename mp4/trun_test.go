5ccd2c44113a2c8ee299582a547df8e905a9a8b4
Torbjörn Einarsson <torbjorn.einarsson@edgeware.tv>
2020-12-28 00:05:43 +0100

    test: add test_file for trun_dump

---
+++ mp4/trun_test.go
@@ -0,0 +1,28 @@
+package mp4
+
+import (
+	"testing"
+)
+
+// TestTrunDump versus golden file. Can be regenerated with -update
+func TestTrunInfo(t *testing.T) {
+	goldenDumpPath := "testdata/golden_trun_dump.txt"
+	trun := CreateTrun()
+	trun.DataOffset = 314159
+	fs := FullSample{
+		Sample: Sample{
+			Flags: SyncSampleFlags,
+			Dur:   1024,
+			Size:  4,
+			Cto:   -512,
+		},
+		DecodeTime: 1024,
+		Data:       []byte{0, 1, 2, 3},
+	}
+	trun.AddFullSample(&fs)
+
+	err := compareOrUpdateInfo(t, trun, goldenDumpPath)
+	if err != nil {
+		t.Error(err)
+	}
+}
