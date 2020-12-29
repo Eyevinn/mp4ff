f641a3cfd76e4a28bd84c05eba33705409f6a3f4
Michael Hand <palmdalian@gmail.com>
2020-12-28 20:36:51 -0800

    Remove test code

--- mp4/sdtp_test.go
+++ mp4/sdtp_test.go
@@ -1,15 +1,14 @@
 package mp4
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSdtp(t *testing.T) {
 	entries := []SdtpEntry{
 		SdtpEntry(32),
 		SdtpEntry(16),
 		SdtpEntry(24),
-	}
-	for _, entry := range entries {
-		entry.Info()
 	}
 
 	boxDiffAfterEncodeAndDecode(t, CreateSdtpBox(entries))
