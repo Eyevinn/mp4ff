58b31723b573b75a51c749a791cde733c6ffb786
Torbjörn Einarsson <torbjorn.einarsson@edgeware.tv>
2021-01-14 22:41:11 +0100

    feat: mp4ff-pslister hevc + json printout

--- cmd/mp4ff-pslister/main.go
+++ cmd/mp4ff-pslister/main.go
@@ -1,10 +1,11 @@
-// mp4ff-pslister lists parameter sets for H.264/AVC video in mp4 files.
+// mp4ff-pslister lists parameter sets for AVC(H.264) and HEVC(H.265) video in mp4 files.
 //
 // It prints them as hex and with verbose mode it also interprets them.
 package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -12,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/edgeware/mp4ff/avc"
+	"github.com/edgeware/mp4ff/hevc"
 	"github.com/edgeware/mp4ff/mp4"
 )
 
@@ -26,13 +28,13 @@ var Usage = func() {
 	parts := strings.Split(os.Args[0], "/")
 	name := parts[len(parts)-1]
 	fmt.Fprintln(os.Stderr, usg)
-	fmt.Fprintf(os.Stderr, "%s [-vui] [-v] <mp4File>\n", name)
+	fmt.Fprintf(os.Stderr, "%s [-codec hevc] [-v] <mp4File>\n", name)
 	flag.PrintDefaults()
 }
 
 func main() {
-	fullVUI := flag.Bool("vui", false, "Parse full VUI")
 	verbose := flag.Bool("v", false, "Verbose output")
+	codec := flag.String("c", "avc", "Codec to parse (avc or hevc or auto)")
 
 	flag.Parse()
 
@@ -60,54 +62,78 @@ func main() {
 	for _, trak := range parsedMp4.Moov.Traks {
 		if trak.Mdia.Hdlr.HandlerType == "vide" {
 			stsd := trak.Mdia.Minf.Stbl.Stsd
-			if stsd.AvcX == nil {
+			if stsd.AvcX != nil {
+				*codec = "avc"
+			} else if stsd.HvcX != nil {
+				*codec = "hevc"
+			} else {
 				continue
 			}
 			found = true
-			avcC := stsd.AvcX.AvcC
 			trackID := trak.Tkhd.TrackID
 			if *verbose {
-				fmt.Printf("Video track ID=%d\n", trackID)
+				fmt.Printf("Video %s track ID=%d\n", *codec, trackID)
 			}
-			var spsInfo *avc.SPS
-			var err error
-			for i, sps := range avcC.SPSnalus {
-				hexStr := hex.EncodeToString(sps)
-				length := len(hexStr) / 2
-				spsInfo, err = avc.ParseSPSNALUnit(sps, *fullVUI)
-				if err != nil {
-					fmt.Println("Could not parse SPS")
-					return
-				}
-				nrBytesRead := spsInfo.NrBytesRead
-				if nrBytesRead < length {
-					hexStr = fmt.Sprintf("%s_%s", hexStr[:2*nrBytesRead], hexStr[2*nrBytesRead:])
-				}
-				if *verbose {
-					fmt.Printf("SPS %d len %d: %+v\n", i, length, hexStr)
-					fmt.Printf("%+v\n", spsInfo)
-				} else {
-					fmt.Printf("#SPS_%d_%dB:%+v", i, length, hexStr)
-				}
-			}
-			for i, pps := range avcC.PPSnalus {
-				ppsInfo, err := avc.ParsePPSNALUnit(pps, spsInfo)
-				if err != nil {
-					fmt.Println("Could not parse PPS")
-					return
-				}
-				hexStr := hex.EncodeToString(pps)
-				length := len(hexStr) / 2
-				if *verbose {
-					fmt.Printf("PPS %d len %d: %+v\n", i, length, hexStr)
-					fmt.Printf("%+v\n", ppsInfo)
-				} else {
-					fmt.Printf("#PPS_%d_%dB:%+v", i, length, hexStr)
-				}
+			switch *codec {
+			case "avc":
+				printAvcPS(stsd.AvcX.AvcC, *verbose)
+			case "hevc":
+				printHevcPS(stsd.HvcX.HvcC, *verbose)
 			}
 		}
 	}
 	if !found {
 		fmt.Println("No parsable video track found")
+	}
+}
+
+func printAvcPS(avcC *mp4.AvcCBox, verbose bool) {
+	var spsInfo *avc.SPS
+	var err error
+	for i, sps := range avcC.SPSnalus {
+		spsInfo, err = avc.ParseSPSNALUnit(sps, true /*fullVui*/)
+		if err != nil {
+			fmt.Println("Could not parse SPS")
+			return
+		}
+		printPS("SPS", i+1, sps, spsInfo, verbose)
+	}
+	for i, pps := range avcC.PPSnalus {
+		ppsInfo, err := avc.ParsePPSNALUnit(pps, spsInfo)
+		if err != nil {
+			fmt.Println("Could not parse PPS")
+			return
+		}
+		printPS("PPS", i+1, pps, ppsInfo, verbose)
+	}
+}
+
+func printHevcPS(hvcC *mp4.HvcCBox, verbose bool) {
+	for i, vps := range hvcC.GetNalusForType(hevc.NALU_VPS) {
+		printPS("VPS", i+1, vps, nil, false)
+	}
+	for i, sps := range hvcC.GetNalusForType(hevc.NALU_SPS) {
+		spsInfo, err := hevc.ParseSPSNALUnit(sps)
+		if err != nil {
+			fmt.Println("Could not parse SPS")
+			return
+		}
+		printPS("SPS", i+1, sps, spsInfo, verbose)
+	}
+	for i, pps := range hvcC.GetNalusForType(hevc.NALU_PPS) {
+		printPS("PPS", i+1, pps, nil, false)
+	}
+}
+
+func printPS(name string, nr int, ps []byte, psInfo interface{}, verbose bool) {
+	hexStr := hex.EncodeToString(ps)
+	length := len(hexStr) / 2
+	fmt.Printf("%s %d len %dB: %+v\n", name, nr, length, hexStr)
+	if verbose && psInfo != nil {
+		jsonPS, err := json.MarshalIndent(psInfo, "", "  ")
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		fmt.Printf("%s\n", string(jsonPS))
 	}
 }
