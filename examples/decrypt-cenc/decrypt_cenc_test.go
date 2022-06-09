4e3963adea24cd403f8aa5a3accd298a7e2746b1
Torbjörn Einarsson <torbjorn.einarsson@edgeware.tv>
2022-06-09 09:24:17 +0200

    test: add test for cbcs decrypt

--- examples/decrypt-cenc/decrypt_cenc_test.go
+++ examples/decrypt-cenc/decrypt_cenc_test.go
@@ -8,29 +8,49 @@ import (
 	"github.com/go-test/deep"
 )
 
-func TestDecodeCenc(t *testing.T) {
-	inFile := "../../mp4/testdata/prog_8s_enc_dashinit.mp4"
-	expectedOutFile := "../../mp4/testdata/prog_8s_dec_dashinit.mp4"
-	hexString := "63cb5f7184dd4b689a5c5ff11ee6a328"
-	raw, err := ioutil.ReadFile(inFile)
-	if err != nil {
-		t.Error(err)
+func TestDecryptFiles(t *testing.T) {
+	testCases := []struct {
+		name            string
+		inFile          string
+		expectedOutFile string
+		hexKey          string
+	}{
+		{
+			name:            "cenc",
+			inFile:          "../../mp4/testdata/prog_8s_enc_dashinit.mp4",
+			expectedOutFile: "../../mp4/testdata/prog_8s_dec_dashinit.mp4",
+			hexKey:          "63cb5f7184dd4b689a5c5ff11ee6a328",
+		},
+		{
+			name:            "cbcs",
+			inFile:          "../../mp4/testdata/cbcs.mp4",
+			expectedOutFile: "../../mp4/testdata/cbcsdec.mp4",
+			hexKey:          "22bdb0063805260307ee5045c0f3835a",
+		},
 	}
-	inBuf := bytes.NewBuffer(raw)
-	buf := bytes.Buffer{}
-	err = start(inBuf, &buf, hexString)
-	if err != nil {
-		t.Error(err)
+
+	for _, tc := range testCases {
+		raw, err := ioutil.ReadFile(tc.inFile)
+		if err != nil {
+			t.Error(err)
+		}
+		inBuf := bytes.NewBuffer(raw)
+		buf := bytes.Buffer{}
+		err = start(inBuf, &buf, tc.hexKey)
+		if err != nil {
+			t.Error(err)
+		}
+		expectedOut, err := ioutil.ReadFile(tc.expectedOutFile)
+		if err != nil {
+			t.Error(err)
+		}
+		gotOut := buf.Bytes()
+		diff := deep.Equal(expectedOut, gotOut)
+		if diff != nil {
+			t.Errorf("Mismatch for case %s: %s", tc.name, diff)
+		}
 	}
-	expectedOut, err := ioutil.ReadFile(expectedOutFile)
-	if err != nil {
-		t.Error(err)
-	}
-	gotOut := buf.Bytes()
-	diff := deep.Equal(expectedOut, gotOut)
-	if diff != nil {
-		t.Errorf("Mismatch: %s", diff)
-	}
+
 }
 
 func BenchmarkDecodeCenc(b *testing.B) {
