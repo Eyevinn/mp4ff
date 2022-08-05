b2bc1d48d148a42e2c57e223d38a6e0e97ca4c95
Torbjörn Einarsson <torbjorn.einarsson@edgeware.tv>
2022-08-05 17:13:05 +0200

    feat: add PS extraction from byte stream for both AVC and HEVC

---
+++ hevc/annexb_test.go
@@ -0,0 +1,47 @@
+package hevc
+
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
+func TestGetParameterSetsFromByteStream(t *testing.T) {
+	testCases := []struct {
+		name      string
+		input     []byte
+		wantedVPS [][]byte
+		wantedSPS [][]byte
+		wantedPPS [][]byte
+	}{
+		{
+			"Only IDR",
+			[]byte{0, 0, 0, 1, byte(NALU_IDR_W_RADL) << 1, 0, 0},
+			nil, nil, nil,
+		},
+		{
+			"AUD, VPS, SPS, PPS, IDR",
+			[]byte{0, 0, 0, 1, byte(NALU_AUD) << 1, 2, 0,
+				0, 0, 0, 1, byte(NALU_VPS) << 1, 5, 4,
+				0, 0, 0, 1, byte(NALU_SPS) << 1, 7, 8,
+				0, 0, 0, 1, byte(NALU_PPS) << 1, 1, 2,
+				0, 0, 0, 1, byte(NALU_IDR_W_RADL) << 1, 0},
+			[][]byte{{byte(NALU_VPS) << 1, 5, 4}},
+			[][]byte{{byte(NALU_SPS) << 1, 7, 8}},
+			[][]byte{{byte(NALU_PPS) << 1, 1, 2}},
+		},
+	}
+
+	for _, tc := range testCases {
+		gotVPS, gotSPS, gotPPS := GetParameterSetsFromByteStream(tc.input)
+		if diff := deep.Equal(gotVPS, tc.wantedVPS); diff != nil {
+			t.Errorf("%s: %v", tc.name, diff)
+		}
+		if diff := deep.Equal(gotSPS, tc.wantedSPS); diff != nil {
+			t.Errorf("%s: %v", tc.name, diff)
+		}
+		if diff := deep.Equal(gotPPS, tc.wantedPPS); diff != nil {
+			t.Errorf("%s: %v", tc.name, diff)
+		}
+	}
+}
