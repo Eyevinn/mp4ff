4cf33ea6e8c8f0dc57fdeec204697e40f24c99a0
tanghao <tanghao1996.seu@gmail.com>
2023-09-05 19:13:52 +0800

    test: add ludt test

---
+++ mp4/ludt_test.go
@@ -0,0 +1,15 @@
+package mp4
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+func TestLudt(t *testing.T) {
+	ludtBox := `0000002c6c75647400000024746c6f75010000000100000043f43e2305017923037a13047c13057a13060113`
+	data, err := hex.DecodeString(ludtBox)
+	if err != nil {
+		t.Error(err)
+	}
+	cmpAfterDecodeEncodeBox(t, data)
+}
