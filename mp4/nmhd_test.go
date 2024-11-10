de10571aa654e72b117c0c8a44eca28f57df6b53
Torbjörn Einarsson <torbjorn@einarssons.se>
2024-11-10 07:55:59 +0100

    test: better test coverage for nmhd

--- mp4/nmhd_test.go
+++ mp4/nmhd_test.go
@@ -1,28 +1,11 @@
 package mp4
 
 import (
-	"bytes"
 	"testing"
-
-	"github.com/go-test/deep"
 )
 
 func TestNmhd(t *testing.T) {
 
 	encBox := &NmhdBox{}
-
-	buf := bytes.Buffer{}
-	err := encBox.Encode(&buf)
-	if err != nil {
-		t.Error(err)
-	}
-
-	decBox, err := DecodeBox(0, &buf)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if diff := deep.Equal(encBox, decBox); diff != nil {
-		t.Error(diff)
-	}
+	boxDiffAfterEncodeAndDecode(t, encBox)
 }
