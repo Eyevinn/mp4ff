667580210f8e796583d6aea93f53bc1c3f6a4133
Torbjörn Einarsson <torbjorn@einarssons.se>
2026-07-07 14:29:03 +0200

    test: report panics in HEVC fuzz targets

--- hevc/ps_fuzz_test.go
+++ hevc/ps_fuzz_test.go
@@ -3,12 +3,10 @@ package hevc
 import "testing"
 
 // FuzzParseSPSNALUnit / FuzzParsePPSNALUnit exercise the parameter-set parsers
-// with arbitrary bytes to ensure they always terminate and never panic on
-// malformed input.
+// with arbitrary bytes. Any panic is reported as a fuzzing failure.
 func FuzzParseSPSNALUnit(f *testing.F) {
 	f.Add([]byte{0x42, 0x01, 0x01, 0x01, 0x60, 0x00})
 	f.Fuzz(func(t *testing.T, data []byte) {
-		defer func() { _ = recover() }()
 		_, _ = ParseSPSNALUnit(data)
 	})
 }
@@ -17,7 +15,6 @@ func FuzzParsePPSNALUnit(f *testing.F) {
 	f.Add([]byte{0x44, 0x01, 0x00})
 	spsMap := map[uint32]*SPS{0: {}}
 	f.Fuzz(func(t *testing.T, data []byte) {
-		defer func() { _ = recover() }()
 		_, _ = ParsePPSNALUnit(data, spsMap)
 	})
 }
